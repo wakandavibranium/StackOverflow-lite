113e89dc38d7423deb4ddde12dfc1051fc77510d
kamaumike <kamaumike2013@gmail.com>
2018-09-30 14:32:41 +0300

    Renamed environment variable APP_SETTINGS to FLASK_ENV
    Fixed issue where APP_SETTINGS variable kept picking the default
    environment setting as "production" when running the "flask run" command.
    Renaming variable to FLASK_ENV fixed issue

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ import os
 from app import create_app
 
 # Get the environment settings we setup earlier.In this case it's "development"
-config_name = os.getenv('APP_SETTINGS')
+config_name = os.getenv('FLASK_ENV')
 app = create_app(config_name)
 
 if __name__ == '__main__':
